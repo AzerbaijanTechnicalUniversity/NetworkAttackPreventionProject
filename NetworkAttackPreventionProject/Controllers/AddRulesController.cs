6221fb5ad239131f621aadcbf22632b8dc97cb78
Asif <92951624+asiforucov@users.noreply.github.com>
2022-04-01 21:14:49 +0400

    Add Rules View add

---
+++ NetworkAttackPreventionProject/Controllers/AddRulesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetworkAttackPreventionProject.Controllers
+{
+    public class AddRulesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- NetworkAttackPreventionProject/Controllers/FirewallController.cs
+++ NetworkAttackPreventionProject/Controllers/FirewallController.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace NetworkAttackPreventionProject.Controllers
 {
     public class FirewallController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
